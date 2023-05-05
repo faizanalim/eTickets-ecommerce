fde54de2fb7e5c20b6a9498d0494f8d58209dd2b
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2023-05-06 01:28:03 +0500

    Migration and update db

--- Tickets/Data/AppDbContext.cs
+++ Tickets/Data/AppDbContext.cs
@@ -33,7 +33,7 @@ namespace Tickets.Data
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
-        public DbSet<Producer> Prducers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 	}
 
